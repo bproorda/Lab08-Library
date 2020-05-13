0f6dab31d89e7d848ca40e91b0d50a70524f36eb
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-13 14:47:35 -0500

    added classes for Book and Author

---
+++ Lab08Library/Author.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab08Library
+{
+    public class Author
+    {
+        public Author(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+    }
+}
---
+++ Lab08Library/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab08Library
+{
+    public class Book
+    {
+        public Book(string title, Author author)
+        {
+            this.Title = title;
+            this.bookAuthor = author;
+        }
+
+        public string Title { get; private set; }
+        public Author bookAuthor { get; private set; }
+    }
+}
